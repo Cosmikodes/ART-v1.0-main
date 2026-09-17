8d6889895a33d22dad6f38232a1786fc201f8865
SaikatM <saikatmohantabonga@gmail.com>
2026-09-17 16:21:39 +0530

    Update tcs3200_art.ino

--- examples/tcs3200_art/tcs3200_art.ino
+++ examples/tcs3200_art/tcs3200_art.ino
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include "ART.h"
 
-// TCS230 or TCS3200 pins wiring to Arduino
+// TCS3200 pins wiring to Arduino
 #define S0 4
 #define S1 5
 #define S2 6
 #define S3 7
 #define sensorOut 8
 
-// Stores frequency read by the photodiodes
+// stores frequency from the photodiodes
 int redFrequency = 0;
 int greenFrequency = 0;
 int blueFrequency = 0;
@@ -17,7 +17,7 @@ int blueFrequency = 0;
 const int inputSize = 3;
 const int prototypeSize = 6;
 
-// Define the vigilance parameter (between 0 and 1)
+// Define the vigilance parameter (between 0 and 1 though keep it lower for stability)
 const float vigilance = 0.27;
 
 // Create an instance of the ART network
@@ -33,14 +33,14 @@ void setup() {
   // Setting the sensorOut as an input
   pinMode(sensorOut, INPUT);
 
-  // Setting frequency scaling to 2%
+  // Frequency scaling set to 2% (~10-12 kHz)
   digitalWrite(S0, LOW);
   digitalWrite(S1, HIGH);
 
-  // Begins serial communication
+  // Start the serial communication 9600 baud
   Serial.begin(9600);
 
-  // Initialize the ART network with some sample data
+  // Initialize the ART network with sample data
   float sampleDefault[inputSize] = {71.0, 82.0, 69.0};
   float sampleBlack[inputSize] = {60.0, 62.0, 52.0};
   float sampleWhite[inputSize] = {8.0, 8.0, 7.0};
@@ -57,38 +57,38 @@ void setup() {
 
 }
 void loop() {
-  // Setting RED (R) filtered photodiodes to be read
+  // Setting RED filter
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
 
-  // Reading the output frequency
+  // Reading the output 
   redFrequency = pulseIn(sensorOut, LOW);
 
-  // Printing the RED (R) value
+  // printing RED value
   Serial.print("R = ");
   Serial.print(redFrequency);
   delay(100);
 
-  // Setting GREEN (G) filtered photodiodes to be read
+  // Setting GREEN filter
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
 
-  // Reading the output frequency
+  // Reading the output
   greenFrequency = pulseIn(sensorOut, LOW);
 
-  // Printing the GREEN (G) value
+  // printing GREEN value
   Serial.print(" G = ");
   Serial.print(greenFrequency);
   delay(100);
 
-  // Setting BLUE (B) filtered photodiodes to be read
+  // Setting BLUE filter
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
 
-  // Reading the output frequency
+  // Reading the output 
   blueFrequency = pulseIn(sensorOut, LOW);
 
-  // Printing the BLUE (B) value
+  // Printing BLUE value
   Serial.print(" B = ");
   Serial.println(blueFrequency);
   delay(100);
